5d88a0706f461fc68b398e2a02946e6e4138b95b
meghanm-hub <mmeghan996@gmail.com>
2025-03-10 14:57:21 +0530

    Update and rename app.java to AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Static input
+        int number1 = 10; // First number
+        int number2 = 20; // Second number
+
+        // Adding two numbers
+        int sum = number1 + number2;
+
+        // Output the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
